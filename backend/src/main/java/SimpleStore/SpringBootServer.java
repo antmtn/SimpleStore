b35aafc64f9a73b89c2bb01f71bafc561ed64d7e
antmtn <116135015+notnuthin@users.noreply.github.com>
2025-11-13 22:18:27 -0800

    Initial commit, React/Vite Frontend, Springboot Backend

---
+++ backend/src/main/java/SimpleStore/SpringBootServer.java
@@ -0,0 +1,11 @@
+package SimpleStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServer.class,args);
+    }
+}
