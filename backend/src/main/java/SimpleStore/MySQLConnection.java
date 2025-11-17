45d9fc3bd637e05f0eca8e4cefc94a33ddb9bfef
antmtn <116135015+notnuthin@users.noreply.github.com>
2025-11-17 15:12:22 -0800

    made it so that USER and PASSWORD are from env vars

--- backend/src/main/java/SimpleStore/MySQLConnection.java
+++ backend/src/main/java/SimpleStore/MySQLConnection.java
@@ -5,8 +5,10 @@ import java.sql.*;
 public class MySQLConnection {
 
     private static final String URL  = "jdbc:mysql://localhost:3306/simplestore_db?serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String USER = System.getenv().getOrDefault("DB_USER", "simplestore");
+    private static final String PASSWORD = System.getenv().getOrDefault("DB_PASSWORD", "password");
+
+
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
