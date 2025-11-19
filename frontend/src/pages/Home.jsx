5bd44ea739e5b6ed3167b65aa2d2fcffffbed69c
tofu <isabelleluu0w0@gmail.com>
2025-11-19 04:28:05 -0800

    fetch all products on the frontend (hardcoded values currently)

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+function Home() {
+    return (
+    <Button variant={"contained"} href={'/products'}>Products Here</Button>
+    )
+}
+
+export default Home
