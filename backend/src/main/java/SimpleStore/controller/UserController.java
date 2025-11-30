fa10fde58421e3d513e75efc73fdec3292512ace
tofu <isabelleluu0w0@gmail.com>
2025-11-30 05:18:07 -0800

    change parameters of request function for login/signup

--- backend/src/main/java/SimpleStore/controller/UserController.java
+++ backend/src/main/java/SimpleStore/controller/UserController.java
@@ -1,6 +1,5 @@
 package SimpleStore.controller;
 
-import SimpleStore.model.User;
 import SimpleStore.relations.Users;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,16 +13,28 @@ public class UserController {
     private final Users users = new Users();
 
     @PostMapping("/signup")
-    public ResponseEntity<Integer> signupUser(@RequestBody User user) throws SQLException {
+    public ResponseEntity<Integer> signupUser(@RequestBody UserRequest user) throws SQLException {
         int result = users.checkSignup(user.getUsername(), user.getPassword());
         return ResponseEntity.ok(result);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Integer> loginUser(@RequestBody User user) throws SQLException {
+    public ResponseEntity<Integer> loginUser(@RequestBody UserRequest user) throws SQLException {
         int result = users.checkLogin(user.getUsername(), user.getPassword());
         return ResponseEntity.ok(result);
     }
 
+    public static class UserRequest {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
 
 }
