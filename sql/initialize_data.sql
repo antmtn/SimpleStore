7e7dee499f1368046427895fececc6c3c33a24b0
monica410 <monica94024@gmail.com>
2025-12-03 14:00:43 -0800

    quotes in sql

--- sql/initialize_data.sql
+++ sql/initialize_data.sql
@@ -1,21 +1,21 @@
 source create_schema.sql
 
 INSERT INTO Products (product_id, name, price, quantity, image) VALUES
-    (1, "chocolate", 3.99, 5, "https://cdn11.bigcommerce.com/s-qbjojecpaq/images/stencil/1280x1280/products/442/1968/Untitled_design_-_2023-06-12T155310.493__67118.1686599665.png?c=1"),
-    (2, "shirt", 4.99, 10, "https://m.media-amazon.com/images/I/81-pkxp9h-L._AC_UY1000_.jpg"),
-    (3, "sweater", 13.99, 6, "https://cdni.llbean.net/is/image/wim/505183_1155_41?hei=1095&wid=950&resMode=sharp2&defaultImage=llbprod/505183_0_44"),
-    (4, "shoes", 49.99, 2, "https://www.converse.com/dw/image/v2/BCZC_PRD/on/demandware.static/-/Sites-cnv-master-catalog/default/dw96d51de8/images/a_08/M9160_A_08X1.jpg?sw=406&strip=false"),
-    (5, "water bottle", 10.99, 100, "https://target.scene7.com/is/image/Target/GUEST_5ad40e9f-b69c-45c5-9d3d-7b2704314d2d"),
-    (6, "charger", 14.99, 20, "https://m.media-amazon.com/images/I/41MAlBJ6BhL._AC_UF1000,1000_QL80_.jpg"),
-    (7, "backpack", 30.99, 5, "https://solo-ny.com/cdn/shop/files/UBN795-4_HO_887bba68-33a4-48d5-a1b6-b5947c6164ac.jpg?v=1695140976"),
-    (8, "laptop", 499.99, 6, "https://i5.walmartimages.com/seo/HP-15-6-Touch-Screen-Laptop-Intel-Core-i3-8GB-Memory-256GB-SSD-Silver-Notebook-PC_435571a0-a644-4f27-9e39-19e6c16f76c0.74549be46a6830f4a58dcf9502abc982.jpeg"),
-    (9, "table", 30.99, 5, "https://www.ikea.com/us/en/images/products/mittzon-conference-table-round-birch-veneer-black__1212761_pe910753_s5.jpg"),
-    (10, "chiikawa", 14.99, 30, "https://cdn11.bigcommerce.com/s-89ffd/images/stencil/1280x1280/products/157207/639533/sun-arrow-chiikawa-potetama-plush-chiikawa__66691.1737445026.jpg?c=2?imbypass=on"),
-    (11, "notebook", 4.99, 8, "https://i5.walmartimages.com/seo/Pen-Gear-Wide-Ruled-1-Subject-Notebook-8-x-10-5-Blue-70-Sheets_d212f980-d46b-4e15-b468-f735ba53eb82.9118a770c90e4c2860b4effcfa52399d.jpeg"),
-    (12, "headphones", 19.99, 9, "https://i5.walmartimages.com/seo/VILINICE-Noise-Cancelling-Headphones-Wireless-Bluetooth-Over-Ear-Headphones-with-Microphone-Black-Q8_b994b99c-835f-42fc-8094-9f6be0f9273b.be59955399cdbd1c25011d4a4251ba9b.jpeg"),
-    (13, "mug", 35.99, 4, "https://cdn11.bigcommerce.com/s-dbdycdvfge/images/stencil/1000x1000/products/1253/1579/079-00072-II-2__69127.1650472389.jpg?c=1"),
-    (14, "pen", 2.99, 40, "https://www.pentel.com/cdn/shop/products/glidewrite-signature-ballpoint-pen-3-pkbx930abp3a-337406.jpg?v=1757189011&width=1946"),
-    (15, "keychain", 8.99, 23, "https://omgkawaii.com/cdn/shop/files/omgkawaii-keychains-adorable-bunny-keychain-40751376302293.jpg?v=1701391085&width=900");
+    (1, 'chocolate', 3.99, 5, 'https://cdn11.bigcommerce.com/s-qbjojecpaq/images/stencil/1280x1280/products/442/1968/Untitled_design_-_2023-06-12T155310.493__67118.1686599665.png?c=1'),
+    (2, 'shirt', 4.99, 10, 'https://m.media-amazon.com/images/I/81-pkxp9h-L._AC_UY1000_.jpg'),
+    (3, 'sweater', 13.99, 6, 'https://cdni.llbean.net/is/image/wim/505183_1155_41?hei=1095&wid=950&resMode=sharp2&defaultImage=llbprod/505183_0_44'),
+    (4, 'shoes', 49.99, 2, 'https://www.converse.com/dw/image/v2/BCZC_PRD/on/demandware.static/-/Sites-cnv-master-catalog/default/dw96d51de8/images/a_08/M9160_A_08X1.jpg?sw=406&strip=false'),
+    (5, 'water bottle', 10.99, 100, 'https://target.scene7.com/is/image/Target/GUEST_5ad40e9f-b69c-45c5-9d3d-7b2704314d2d'),
+    (6, 'charger', 14.99, 20, 'https://m.media-amazon.com/images/I/41MAlBJ6BhL._AC_UF1000,1000_QL80_.jpg'),
+    (7, 'backpack', 30.99, 5, 'https://solo-ny.com/cdn/shop/files/UBN795-4_HO_887bba68-33a4-48d5-a1b6-b5947c6164ac.jpg?v=1695140976'),
+    (8, 'laptop', 499.99, 6, 'https://i5.walmartimages.com/seo/HP-15-6-Touch-Screen-Laptop-Intel-Core-i3-8GB-Memory-256GB-SSD-Silver-Notebook-PC_435571a0-a644-4f27-9e39-19e6c16f76c0.74549be46a6830f4a58dcf9502abc982.jpeg'),
+    (9, 'table', 30.99, 5, 'https://www.ikea.com/us/en/images/products/mittzon-conference-table-round-birch-veneer-black__1212761_pe910753_s5.jpg'),
+    (10, 'chiikawa', 14.99, 30, 'https://cdn11.bigcommerce.com/s-89ffd/images/stencil/1280x1280/products/157207/639533/sun-arrow-chiikawa-potetama-plush-chiikawa__66691.1737445026.jpg?c=2?imbypass=on'),
+    (11, 'notebook', 4.99, 8, 'https://i5.walmartimages.com/seo/Pen-Gear-Wide-Ruled-1-Subject-Notebook-8-x-10-5-Blue-70-Sheets_d212f980-d46b-4e15-b468-f735ba53eb82.9118a770c90e4c2860b4effcfa52399d.jpeg'),
+    (12, 'headphones', 19.99, 9, 'https://i5.walmartimages.com/seo/VILINICE-Noise-Cancelling-Headphones-Wireless-Bluetooth-Over-Ear-Headphones-with-Microphone-Black-Q8_b994b99c-835f-42fc-8094-9f6be0f9273b.be59955399cdbd1c25011d4a4251ba9b.jpeg'),
+    (13, 'mug', 35.99, 4, 'https://cdn11.bigcommerce.com/s-dbdycdvfge/images/stencil/1000x1000/products/1253/1579/079-00072-II-2__69127.1650472389.jpg?c=1'),
+    (14, 'pen', 2.99, 40, 'https://www.pentel.com/cdn/shop/products/glidewrite-signature-ballpoint-pen-3-pkbx930abp3a-337406.jpg?v=1757189011&width=1946'),
+    (15, 'keychain', 8.99, 23, 'https://omgkawaii.com/cdn/shop/files/omgkawaii-keychains-adorable-bunny-keychain-40751376302293.jpg?v=1701391085&width=900');
 
 INSERT INTO Users (user_id, username, password) VALUES
     (1,  'mango',      'zest77'),
